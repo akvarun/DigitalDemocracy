60db7975989eb840e5251b5e9b223d8750afa8b1
akvarun <akvarun08@gmail.com>
2024-06-10 12:05:39 +0530

    validation and routing

--- front/src/Home.jsx
+++ front/src/Home.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 import { useState } from 'react';
+import { Link, Navigate } from 'react-router-dom';
 
 function home () {
   const [rollnumber, setRollNumber] = useState('');
+  const [redirect, SetRedirect] = useState('False');
+  const handleInputChange = (event) => {
+    setRollNumber(event.target.value);
+  };
+  const handleVoteClick = ()=>{
+    if(rollnumber.length!=7){
+      alert('Enter a valid Roll number');
+      event.preventDefault();
+      return;
+    }
+    console.log('roll no:',rollnumber);
+    setRollNumber(''); 
+    SetRedirect(true);
+  }
+  if(redirect){
+    return <Navigate to="/HeadVote"/>
+  }
   return (
     <div>
 <div class="px-4 py-5 my-5 text-center">
@@ -11,10 +29,10 @@ function home () {
       <p class="lead mb-5">Voting is a powerful tool in your hands. Your vote shapes the future. Think carefully and choose wisely before casting your vote. Make your voice heard for the leader you believe in.</p>
       <h5>Enter your roll number to start your voting</h5>
       <div class="form-group col-lg-6 mx-auto">
-        <input type="email" class="form-control mt-4" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter your roll number"/>
+        <input type='text' class="form-control mt-4" id="inputRollno"  placeholder="Enter your roll number" value={rollnumber} onChange={handleInputChange}/>
       </div>
       <div class="d-grid gap-2 d-sm-flex justify-content-sm-center mt-3">
-        <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Vote</button>
+        <button type='button' className='btn btn-primary btn-lg px-4 gap-3' onClick={handleVoteClick}>Vote</button>
       </div>
     </div>
   </div>
